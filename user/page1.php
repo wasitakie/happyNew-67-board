48bfb67dde7595dcd3a668b0d148458008f05133
koo21 <sawaddiju@hotmail.com>
2024-10-24 15:00:34 +0700

    kk

--- user/page1.php
+++ user/page1.php
@@ -25,7 +25,7 @@ include '../components/layoutHead.php';
                         <div class="">
                             <label for="first_name"
                                 class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">โปรดระบุปีที่องค์กร/สถานประกอบการก่อตั้ง
-                                (พ.ศ.)</label>
+                                (พ.ศ.)55</label>
                             <input type="text" id="first_name"
                                 class="w-2/5 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
                         </div>
